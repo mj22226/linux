9ef1f29784854ac97c71d30472b2adda46fff6bb
Sav <62543096+dongioia@users.noreply.github.com>
2026-04-07 13:26:00 +0200

    media: rkvdec: add VP9 VDPU381 decoder support
    Add VP9 stateless decoding for VDPU381 (RK3588/RK3576).
    
    Based on work by D.V.A.B. Sarma (dvab-sarma), adapted for mainline
    rkvdec driver framework. VP9 Profile 0, tested up to 1080p.
    
    Known limitation: visual artifacts at resolutions above 1080p (2K/4K).
    Upstream VP9 VDPU381 support from Collabora is listed as future work.
    
    Source: https://github.com/dvab-sarma/android_kernel_rk_opi
    (cherry picked from commit 37e563736d7ebdb51ec9de6b256074da4fb1d6b4)

--- drivers/media/platform/rockchip/rkvdec/rkvdec.c
+++ drivers/media/platform/rockchip/rkvdec/rkvdec.c
@@ -292,6 +292,31 @@ static const struct rkvdec_ctrls vdpu38x_hevc_ctrls = {
 	.num_ctrls = ARRAY_SIZE(vdpu38x_hevc_ctrl_descs),
 };
 
+static const struct rkvdec_ctrl_desc vdpu381_vp9_ctrl_descs[] = {
+	{
+		.cfg.id = V4L2_CID_STATELESS_VP9_FRAME,
+	},
+	{
+		.cfg.id = V4L2_CID_STATELESS_VP9_COMPRESSED_HDR,
+	},
+	{
+		.cfg.id = V4L2_CID_MPEG_VIDEO_VP9_PROFILE,
+		.cfg.min = V4L2_MPEG_VIDEO_VP9_PROFILE_0,
+		.cfg.max = V4L2_MPEG_VIDEO_VP9_PROFILE_0,
+		.cfg.def = V4L2_MPEG_VIDEO_VP9_PROFILE_0,
+	},
+	{
+		.cfg.id = V4L2_CID_MPEG_VIDEO_VP9_LEVEL,
+		.cfg.min = V4L2_MPEG_VIDEO_VP9_LEVEL_1_0,
+		.cfg.max = V4L2_MPEG_VIDEO_VP9_LEVEL_6_1,
+	},
+};
+
+static const struct rkvdec_ctrls vdpu381_vp9_ctrls = {
+	.ctrls = vdpu381_vp9_ctrl_descs,
+	.num_ctrls = ARRAY_SIZE(vdpu381_vp9_ctrl_descs),
+};
+
 static const struct rkvdec_decoded_fmt_desc rkvdec_hevc_decoded_fmts[] = {
 	{
 		.fourcc = V4L2_PIX_FMT_NV12,
@@ -541,6 +566,22 @@ static const struct rkvdec_coded_fmt_desc vdpu381_coded_fmts[] = {
 		.ops = &rkvdec_vdpu381_h264_fmt_ops,
 		.num_decoded_fmts = ARRAY_SIZE(rkvdec_h264_decoded_fmts),
 		.decoded_fmts = rkvdec_h264_decoded_fmts,
+		.subsystem_flags = VB2_V4L2_FL_SUPPORTS_M2M_HOLD_CAPTURE_BUF,
+	},
+	{
+		.fourcc = V4L2_PIX_FMT_VP9_FRAME,
+		.frmsize = {
+			.min_width = 64,
+			.max_width = 65472,
+			.step_width = 64,
+			.min_height = 64,
+			.max_height = 65472,
+			.step_height = 64,
+		},
+		.ctrls = &vdpu381_vp9_ctrls,
+		.ops = &rkvdec_vdpu381_vp9_fmt_ops,
+		.num_decoded_fmts = ARRAY_SIZE(rkvdec_vp9_decoded_fmts),
+		.decoded_fmts = rkvdec_vp9_decoded_fmts,
 		.subsystem_flags = VB2_V4L2_FL_SUPPORTS_M2M_HOLD_CAPTURE_BUF,
 	},
 };
