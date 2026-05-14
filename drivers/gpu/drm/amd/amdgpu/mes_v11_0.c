1e9819678f27417942e4edabb3a4f567ec635e3a
Alex Deucher <alexander.deucher@amd.com>
2026-05-14 15:29:29 -0400

    drm/amdgpu/mes11: enable compute MMIO pipe reset
    Enable MMIO pipe reset for compute pipes.
    
    Reviewed-by: Jesse Zhang <jesse.zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/mes_v11_0.c
+++ drivers/gpu/drm/amd/amdgpu/mes_v11_0.c
@@ -478,9 +478,6 @@ static int mes_v11_0_reset_compute_pipe_mmio(struct amdgpu_device *adev,
 	uint32_t reset_val, clean_val;
 	int r;
 
-	if (!mes_v11_0_pipe_reset_support(adev))
-		return -EOPNOTSUPP;
-
 	amdgpu_gfx_rlc_enter_safe_mode(adev, 0);
 	mutex_lock(&adev->srbm_mutex);
 	soc21_grbm_select(adev, me, pipe, queue, 0);
