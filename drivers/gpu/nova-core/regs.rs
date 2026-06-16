9102e655ea7285c1bc329669865ba8a38cdb69e6
Antonin Malzieu Ridolfi <dev@nanonej.com>
2026-06-17 01:48:12 +0200

    gpu: nova-core: fb: Move PDISP register definition
    Move PDISP register definition into fb module and update register
    visibility.
    
    Signed-off-by: Antonin Malzieu Ridolfi <dev@nanonej.com>
    Link: https://patch.msgid.link/20260617-nova-core-regs-split-v1-1-4c7dc4450ea7@nanonej.com
    [acourbot: fix rustfmt issue.]
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- drivers/gpu/nova-core/regs.rs
+++ drivers/gpu/nova-core/regs.rs
@@ -302,28 +302,6 @@ impl NV_USABLE_FB_SIZE_IN_MB {
     }
 }
 
-// PDISP
-
-register! {
-    pub(crate) NV_PDISP_VGA_WORKSPACE_BASE(u32) @ 0x00625f04 {
-        /// VGA workspace base address divided by 0x10000.
-        31:8    addr;
-        /// Set if the `addr` field is valid.
-        3:3     status_valid => bool;
-    }
-}
-
-impl NV_PDISP_VGA_WORKSPACE_BASE {
-    /// Returns the base address of the VGA workspace, or `None` if none exists.
-    pub(crate) fn vga_workspace_addr(self) -> Option<u64> {
-        if self.status_valid() {
-            Some(u64::from(self.addr()) << 16)
-        } else {
-            None
-        }
-    }
-}
-
 // FUSE
 
 pub(crate) const NV_FUSE_OPT_FPF_SIZE: usize = 16;
